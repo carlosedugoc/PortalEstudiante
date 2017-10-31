aadfb9d19364c71d8499d3d5a8f33ee5924f24d4
Brahiam David Gaona Muñoz <Bgaona@intergrupo.com>
2017-10-31 15:15:05 -0500

    PE 18 : Actualización para cargar información

--- src/app/components/university-management/university-management.component.ts
+++ src/app/components/university-management/university-management.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UniversityService } from '../../services/university.service';
 import { University } from "../../models/university";
+import { List } from 'linqts';
 
 @Component({
   selector: 'app-university-management',
@@ -9,7 +10,10 @@ import { University } from "../../models/university";
 })
 export class UniversityManagementComponent implements OnInit {
 
-  public universities: University[]
+  public universities: List<University> = new List<University>()
+  public uniOriginal: List<University> = new List<University>()
+  public uniToCreate: List<University> = new List<University>()
+  public uniToUpdate: List<University> = new List<University>()
 
   constructor(private universityService: UniversityService
   ) {
@@ -19,8 +23,25 @@ export class UniversityManagementComponent implements OnInit {
     this.cargarInformacionUniversidades()
   }
 
+  //// Método que me permite verificar que información se crea y que información se actualiza.
+  guardarInformacion() {
+    debugger;
+    let arr = new List<number>([1, 2, 3, 4, 5])
+      .Where(x => x > 3)
+      .Select(y => y * 2)
+      .ToArray(); // > [8, 10]
+
+    let diferents = new List<University>()
+    diferents = this.uniOriginal.Except(this.universities).ToList()
+  }
+
   cargarInformacionUniversidades() {
-    this.universityService.getInfoAllUniversities().subscribe(res => this.universities = res)
+    this.universityService.getInfoAllUniversities().subscribe(res => {
+      this.universities = res
+      // Se realiza una copia de manera que la información original la tenga almacenada en memoria.
+      let unis: string = JSON.stringify(res)
+      this.uniOriginal = Object.create(JSON.parse(unis));
+    })
   }
 
   crearUniversidad() {
@@ -37,5 +58,10 @@ export class UniversityManagementComponent implements OnInit {
     universityToUpdate.name = "Nuevo nombre"
     universityToUpdate.status = !universityToUpdate.status
     this.universityService.updateUniversity(universityToUpdate).subscribe(res => console.log(res));
+  }
+
+  //// Método que devuelve los valores a su estado inicial.
+  cancelar() {
+
   }
 }
